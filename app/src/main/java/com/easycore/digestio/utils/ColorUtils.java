85981284f0738ed97823686f566a65585b4ac7d3
Michal Kejzlar <kejzlarmichal@gmail.com>
2017-02-02 20:30:24 +0100

    Remove unused libs

--- app/src/main/java/com/easycore/digestio/utils/ColorUtils.java
+++ app/src/main/java/com/easycore/digestio/utils/ColorUtils.java
@@ -16,15 +16,11 @@
 
 package com.easycore.digestio.utils;
 
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
 import android.support.annotation.FloatRange;
 import android.support.annotation.IntDef;
 import android.support.annotation.IntRange;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.graphics.Palette;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -73,55 +69,6 @@ public class ColorUtils {
         return Color.argb((int) a, (int) r, (int) g, (int) b);
     }
 
-    /**
-     * Checks if the most populous color in the given palette is dark
-     * <p/>
-     * Annoyingly we have to return this Lightness 'enum' rather than a boolean as palette isn't
-     * guaranteed to find the most populous color.
-     */
-    public static @Lightness int isDark(Palette palette) {
-        Palette.Swatch mostPopulous = getMostPopulousSwatch(palette);
-        if (mostPopulous == null) return LIGHTNESS_UNKNOWN;
-        return isDark(mostPopulous.getHsl()) ? IS_DARK : IS_LIGHT;
-    }
-
-    public static @Nullable
-    Palette.Swatch getMostPopulousSwatch(Palette palette) {
-        Palette.Swatch mostPopulous = null;
-        if (palette != null) {
-            for (Palette.Swatch swatch : palette.getSwatches()) {
-                if (mostPopulous == null || swatch.getPopulation() > mostPopulous.getPopulation()) {
-                    mostPopulous = swatch;
-                }
-            }
-        }
-        return mostPopulous;
-    }
-
-    /**
-     * Determines if a given bitmap is dark. This extracts a palette inline so should not be called
-     * with a large image!!
-     * <p/>
-     * Note: If palette fails then check the color of the central pixel
-     */
-    public static boolean isDark(@NonNull Bitmap bitmap) {
-        return isDark(bitmap, bitmap.getWidth() / 2, bitmap.getHeight() / 2);
-    }
-
-    /**
-     * Determines if a given bitmap is dark. This extracts a palette inline so should not be called
-     * with a large image!! If palette fails then check the color of the specified pixel
-     */
-    public static boolean isDark(@NonNull Bitmap bitmap, int backupPixelX, int backupPixelY) {
-        // first try palette with a small color quant size
-        Palette palette = Palette.from(bitmap).maximumColorCount(3).generate();
-        if (palette != null && palette.getSwatches().size() > 0) {
-            return isDark(palette) == IS_DARK;
-        } else {
-            // if palette failed, then check the color of the specified pixel
-            return isDark(bitmap.getPixel(backupPixelX, backupPixelY));
-        }
-    }
 
     /**
      * Check that the lightness value (0–1)
